8d39c5aaf96d13ca0b83cb8551c9dc640034bffe
Paul Romeo <promeo24@gmail.com>
2023-12-11 21:06:24 -0500

    Started working with Kivy

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import weather_app
+from weather_app import WeatherApp
+
+
+def getZipCode():
+    while True:
+        zip_code = input("Enter the zip for a forecast: ")
+        if len(zip_code) == 5 and zip_code.isnumeric():
+            return zip_code
+        else:
+            print("Please enter a valid zip code")
+
 
 api_key = '0bec28f01706819cc9e739dd721e8f12'
-app = weather_app.WeatherApp(api_key)
-zip_code = app.getZipCode()
+base_url = "http://api.weatherstack.com/"
+app = weather_app.WeatherApp(api_key, base_url)
+zip_code = getZipCode()
 print(f"You entered zip code: {zip_code}")
 app.getCurrentWeather(zip_code)
-
--- weather_app.py
+++ weather_app.py
@@ -2,21 +2,34 @@
 A simple weather app, enter a zip code and get a forecast!
 '''
 
-import requests
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+import requests
 
-class WeatherApp:
-    def __init__(self, api_key):
-        self.api_key = api_key
+class WeatherAppLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+        self.api_key = '0bec28f01706819cc9e739dd721e8f12'
         self.base_url = "http://api.weatherstack.com/"
 
-    def getZipCode(self):
-        while True:
-            zip_code = input("Enter the zip for a forecast: ")
-            if len(zip_code) == 5:
-                return zip_code
-            else:
-                print("Please enter a valid zip code")
+        self.zip_code = TextInput(multiline=False)
+        self.add_widget(Label(text="Enter Zip Code:"))
+        self.add_widget(self.zip_code)
+
+        self.weather_display = Label(text='')
+        self.add_widget(self.weather_display)
+
+        self.get_weather_button
+
+
+class WeatherApp:
+    def __init__(self, api_key, base_url):
+        self.api_key = api_key
+        self.base_url = base_url
 
     def getCurrentWeather(self, zip_code):
         url = f"{self.base_url}forecast?access_key={self.api_key}&query={zip_code}"
