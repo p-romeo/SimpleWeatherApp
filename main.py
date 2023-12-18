03eed71ba879a633b7f1f9276860fbeaaaa975b5
Paul Romeo <promeo98@proton.me>
2023-12-18 13:18:37 -0500

    Merge weather_app.py into main.py
    Consolidated the codebase by deleting the weather_app.py file and incorporating its functionality into main.py. This aims to enhance readability and maintenance by having all application logic in a central location. API key and zip code validation were included in this transfer.

--- main.py
+++ main.py
@@ -1,15 +1,70 @@
-import weather_app
+'''
+A simple weather app, enter a zip code and get a forecast!
+'''
 
-def getZipCode():
-    while True:
-        zip_code = input("Enter the zip for a forecast: ")
-        if len(zip_code) == 5 and zip_code.isnumeric():
-            return zip_code
-        else:
-            print("Please enter a valid zip code")
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
-api_key = '3fd394fd91eb46557abe94f7fefb5e3e'
-base_url = "http://api.weatherstack.com/"
-zip_code = getZipCode()
-print(f"You entered zip code: {zip_code}")
+api_key = os.getenv('weather_stack_api_key')
+base_url = 'http://api.weatherstack.com/'
+
+class WeatherAppLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+        self.api_key = api_key
+        self.base_url = base_url
+
+        self.zip_code = TextInput(multiline=False)
+        self.zip_code.bind(on_text_validate=self.get_weather)
+        self.add_widget(Label(text="Enter Zip Code:"))
+        self.add_widget(self.zip_code)
+
+        self.weather_display = Label(text='')
+        self.add_widget(self.weather_display)
+
+        self.get_weather_button = Button(text='Get Weather')
+        self.get_weather_button.bind(on_press=self.get_weather)
+        self.add_widget(self.get_weather_button)
+
+
+    def get_weather(self, instance):
+        zip_code = self.zip_code.text
+        if len(zip_code) != 5 or not zip_code.isnumeric():
+            self.weather_display.text = "Invalid Zip Code"
+            return
+        url = f"{self.base_url}current?access_key={self.api_key}&query={zip_code}"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except Exception as err:
+            self.weather_display.text = f'An error occurred: {err}'
+            return
+
+        data = response.json()
+
+        temperature = data['current']['temperature']
+        fahrenheit = (temperature * 9/5) + 32
+
+        self.weather_display.text = (f"Weather at {data['location']['name']} \n"
+                                     f"{data['location']['localtime']}: \n"
+                                     f"{fahrenheit} degrees, \n"
+                                     f" {data['current']['weather_descriptions']}")
+
+
+class WeatherApp(App):
+    def build(self):
+        return WeatherAppLayout()
+
+
+if __name__ == '__main__':
+    WeatherApp().run()
